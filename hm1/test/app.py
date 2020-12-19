3d4c19844d5e2205c9814e21bcbd6224c12c9a3e
Rusnak Alexandr <rus91ad@mail.ru>
2020-12-19 17:59:46 +0700

    new content

--- hm1/test/app.py
+++ hm1/test/app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from string import Template
+import random
+from favorite_list import links
+
+HTML_TEMPLATE = Template("""
+<iframe width="1280" height="720" src="https://www.youtube.com/embed/${youtube_id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+""")
+
 app = Flask(__name__)
- 
+
 @app.route('/')
-def hello_world():
-   return 'Launch successful.'
- 
+def homepage():
+    vid = random.choice(links)
+    return HTML_TEMPLATE.substitute(youtube_id=vid)
+
+@app.route('/videos/<vid>')
+def videos(vid):
+    return HTML_TEMPLATE.substitute(youtube_id=random.choice(links))
+
 if __name__ == '__main__':
-   app.run(port=80, debug=True, host='0.0.0.0')
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(port=80, debug=True, host='0.0.0.0')
+
+
+
---
+++ hm1/test/favorite_list.py
@@ -0,0 +1,5 @@
+links = [
+	'EcDw3X4ImUU',
+	'As03tlODkdw',
+]
+
